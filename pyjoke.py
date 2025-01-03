ec7755eba1a3d6f40f392eb28b98ce74287efd3e
PatrioticJetpack90 <ijonesal3@gmail.com>
2025-01-03 13:25:17 -0500

    Create pyjoke.py

---
+++ pyjoke.py
@@ -0,0 +1,11 @@
+import pyjokes
+
+total_jokes = int(input("Number of jokes: "))
+
+num_jokes = 1
+
+while num_jokes < total_jokes:
+    print(f"{num_jokes} {pyjokes.get_joke()}")
+
+    num_jokes += 1 
+    
