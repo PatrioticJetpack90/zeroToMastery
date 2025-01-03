978f0681c19b5aa0a92cf0fba2bd24ce86a21e02
PatrioticJetpack90 <ijonesal3@gmail.com>
2025-01-03 13:26:18 -0500

    Update pyjoke.py

--- pyjoke.py
+++ pyjoke.py
@@ -4,8 +4,6 @@ total_jokes = int(input("Number of jokes: "))
 
 num_jokes = 1
 
-while num_jokes < total_jokes:
+while num_jokes <= total_jokes:
     print(f"{num_jokes} {pyjokes.get_joke()}")
-
-    num_jokes += 1 
-    
+    num_jokes += 1
